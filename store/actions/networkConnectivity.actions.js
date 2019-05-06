03c306f50c47a39f764b30cae308e704a6b24be1
zdmc23 <191707+zdmc23@users.noreply.github.com>
2019-05-06 00:12:24 -0400

    remove debug logs

--- store/actions/networkConnectivity.actions.js
+++ store/actions/networkConnectivity.actions.js
@@ -1,9 +1,7 @@
 export function setNetworkConnectivity(isConnected) {
   if (isConnected) {
-    console.log("*** BACK ONLINE! ***")
     return { type: 'ONLINE' }
   }
-  console.log("*** GOING OFFLINE... ***")
   return { type: 'OFFLINE' }
 }
 
--- store/reducers/networkConnectivity.reducer.js
+++ store/reducers/networkConnectivity.reducer.js
@@ -5,10 +5,8 @@ const initialState = {
 export default function networkConnectivityReducer(state = initialState, action) {
   switch (action.type) {
     case 'ONLINE':
-      console.log("^^^ ONLINE ^^^")
       return { ...state, isConnected: true }
     case 'OFFLINE':
-      console.log("^^^ OFFLINE ^^^")
       return { ...state, isConnected: false }
     default:
       return state
