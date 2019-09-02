610f4cf9a9f07078ceb680c4d56a4b245d29736c
Hans Rasch <hans.rasch@highdeveloper.com>
2019-09-02 07:44:30 -0500

    Implemented improvements to requests.sagas, updated response capture in sagas files. Updated contact 'geonames' property to 'location_grid'.

--- store/sagas/request.sagas.js
+++ store/sagas/request.sagas.js
@@ -43,13 +43,13 @@ function* sendRequest(url, data) {
 }
 
 function* processRequest(request) {
-  //console.log('request: ', request.payload.url);
+  // console.log('request: ', request.payload.url);
   const { response, timeout } = yield race({
     response: call(sendRequest, request.payload.url, request.payload.data),
     timeout: delay(REQUEST_TIMEOUT_MILLIS),
   });
   if (response) {
-    //console.log(`response: ${request.payload.url}`);
+    // console.log(`response: ${request.payload.url}`);
     if (request.payload.action) {
       // console.log("response", response);
       yield put({ type: request.payload.action, payload: response });
@@ -70,13 +70,13 @@ export default function* requestSaga() {
       offline: take(offlineChannel),
       request: take(requestChannel),
     });
-    //console.log('request', request);
-    //console.log('offline', offline);
+    // console.log('request', request);
+    // console.log('offline', offline);
     if (request) {
       // ONLINE request
       // Get current queue, compare it whit last request (if exist, fork it)
       const queue = yield select(state => state.requestReducer.queue);
-      //console.log('queue', queue);
+      // console.log('queue', queue);
       for (const action of queue) {
         if (action === request) {
           // process the request
@@ -86,7 +86,7 @@ export default function* requestSaga() {
     } else if (offline) {
       // Get last request
       const { payload } = yield select(state => state.requestReducer.currentAction);
-      //console.log('payload', payload);
+      // console.log('payload', payload);
       // OFFLINE request
       if (payload && payload.data.method === 'POST' && payload.action.includes('SAVE')) {
         // Offline entity creation (send "last request" as response)
