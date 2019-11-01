54167b1bfbd082414ab3f2474c8e0bf1690cf005
Hans Rasch <hans.rasch@highdeveloper.com>
2019-11-01 16:39:22 -0500

    Implemented fab component to show icons with labels.

--- constants/Colors.js
+++ constants/Colors.js
@@ -1,6 +1,6 @@
 const tintColor = '#365D86';
 const accent = '#a0ce4e';
-
+const tintColorRGBA = 'rgba(54,93,134,1)';
 const grayLight = '#eeeeee';
 const gray = '#cccccc';
 const grayDark = '#666666';
@@ -9,7 +9,7 @@ export default {
   primary: tintColor,
   accent,
   tintColor,
-
+  primaryRGBA: tintColorRGBA,
   grayLight,
   gray,
   grayDark,
