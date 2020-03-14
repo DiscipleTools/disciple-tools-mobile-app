8af95a07b0ff4d296d8b394a4cbbc6b144cd5321
zdmc23 <191707+zdmc23@users.noreply.github.com>
2020-03-14 14:27:31 -0400

    Include latest language translations in anticipation of release

--- languages/index.js
+++ languages/index.js
@@ -4,15 +4,27 @@ import i18n from 'i18n-js';
 
 import * as en from './en.json';
 import * as ar from './ar.json';
-import * as bn from './bn.json';
 import * as es from './es.json';
+import * as fa from './fa.json';
 import * as fr from './fr.json';
+import * as pt from './pt.json';
+import * as ru from './ru.json';
 import * as tr from './tr.json';
+import * as zhCn from './zh-cn.json';
 import * as zhTw from './zh-tw.json';
 
 i18n.fallbacks = true;
 i18n.translations = {
-  en, ar, bn, es, fr, tr, zhTw,
+  en,
+  ar,
+  es,
+  fa,
+  fr,
+  pt,
+  ru,
+  tr,
+  zhCn,
+  zhTw,
 };
 i18n.locale = Localization.locale;
 i18n.isRTL = I18nManager.isRTL;
--- languages/locales.js
+++ languages/locales.js
@@ -1,29 +1,52 @@
-export default [{
-  code: 'en',
-  name: 'English',
-  direction: 'ltr',
-}, {
-  code: 'ar',
-  name: 'عربي',
-  direction: 'rtl',
-}, {
-  code: 'es',
-  name: 'Español',
-  direction: 'ltr',
-}, {
-  code: 'bn',
-  name: 'বাংলা',
-  direction: 'ltr',
-}, {
-  code: 'fr',
-  name: 'Français',
-  direction: 'ltr',
-}, {
-  code: 'tr',
-  name: 'Türk',
-  direction: 'ltr',
-}, {
-  code: 'zh-tw',
-  name: '中文-台灣',
-  direction: 'ltr',
-}];
+export default [
+  {
+    code: 'en',
+    name: 'English',
+    direction: 'ltr',
+  },
+  {
+    code: 'ar',
+    name: 'عربي',
+    direction: 'rtl',
+  },
+  {
+    code: 'es',
+    name: 'Español',
+    direction: 'ltr',
+  },
+  {
+    code: 'fa',
+    name: 'فارسی',
+    direction: 'rtl',
+  },
+  {
+    code: 'fr',
+    name: 'Français',
+    direction: 'ltr',
+  },
+  {
+    code: 'pt',
+    name: 'Português',
+    direction: 'ltr',
+  },
+  {
+    code: 'ru',
+    name: 'русский язык',
+    direction: 'ltr',
+  },
+  {
+    code: 'tr',
+    name: 'Türk',
+    direction: 'ltr',
+  },
+  {
+    code: 'zh-cn',
+    name: '汉语',
+    direction: 'ltr',
+  },
+  {
+    code: 'zh-tw',
+    name: '漢語',
+    direction: 'ltr',
+  },
+];
