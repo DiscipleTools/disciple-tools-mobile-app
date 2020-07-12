a14c62d747059f4cfdaaa854c2b327bf5ecde2ce
zdmc23 <191707+zdmc23@users.noreply.github.com>
2020-07-12 17:41:07 -0400

    Try again with Storybook asyncStorage: null

---
+++ storybook/index.js
@@ -0,0 +1,17 @@
+import { getStorybookUI, configure } from '@storybook/react-native';
+
+import './rn-addons';
+
+// import stories
+configure(() => {
+  require('./stories');
+}, module);
+
+// Refer to https://github.com/storybookjs/storybook/tree/master/app/react-native#start-command-parameters
+// To find allowed options for getStorybookUI
+const StorybookUIRoot = getStorybookUI({
+  disableWebsockets: true,
+  asyncStorage: null,
+});
+
+export default StorybookUIRoot;
---
+++ storybook/stories/Welcome/index.stories.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react-native';
+import Welcome from './index';
+
+storiesOf('Welcome', module).add('to Storybook', () => <Welcome />);
