556d062d7d10b4ebb7c37dccf884bb335495df92
Mike Allbutt <yo@ur.id.au>
2020-09-04 04:22:19 -0500

    Removed 1 line of un-needed code

--- constants/Layout.js
+++ constants/Layout.js
@@ -1,7 +1,6 @@
 import { Dimensions } from 'react-native';
 
 const { width, height } = Dimensions.get('window');
-const marginAuto = { marginTop: 'auto', marginTop: 'auto' };
 
 export default {
   window: {
