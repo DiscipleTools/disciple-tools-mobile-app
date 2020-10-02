ca1fa0dd72972c7fe004583f49ba6345313239ff
Hans Smüller Rasch Diaz <raschdiaz@gmail.com>
2020-10-02 09:34:05 -0500

    Sprint 27 (#470)
    * Resolves #355

--- store/actions/i18n.actions.js
+++ store/actions/i18n.actions.js
@@ -2,6 +2,8 @@
  * Action Types
  */
 export const I18N_SETLANGUAGE = 'I18N_SETLANGUAGE';
+export const I18N_CANCEL_SET_LANGUAGE = 'I18N_CANCEL_SET_LANGUAGE';
+export const I18N_SET_CANCEL_FALSE = 'I18N_SET_CANCEL_FALSE';
 
 /*
  * Action Creators
@@ -11,5 +13,17 @@ export function setLanguage(locale, isRTL) {
     type: I18N_SETLANGUAGE,
     locale,
     isRTL,
+  };
+}
+
+export function cancelSetLanguage() {
+  return {
+    type: I18N_CANCEL_SET_LANGUAGE,
+  };
+}
+
+export function setCancelFalse() {
+  return {
+    type: I18N_SET_CANCEL_FALSE,
   };
 }
--- store/reducers/i18n.reducer.js
+++ store/reducers/i18n.reducer.js
@@ -1,19 +1,48 @@
 import * as actions from '../actions/i18n.actions';
 
 const initialState = {
+  canceledLocaleChange: false,
+  previousLocale: null,
+  previousIsRTL: false,
   locale: null,
   isRTL: false,
 };
 
 export default function i18nReducer(state = initialState, action) {
+  let newState = {
+    ...state,
+  };
   switch (action.type) {
     case actions.I18N_SETLANGUAGE: {
+      if (action.locale !== state.locale) {
+        return {
+          ...newState,
+          previousLocale: state.locale,
+          previousIsRTL: state.isRTL,
+          locale: action.locale,
+          isRTL: action.isRTL,
+        };
+      } else {
+        return newState;
+      }
+    }
+    case actions.I18N_CANCEL_SET_LANGUAGE: {
       return {
-        locale: action.locale,
-        isRTL: action.isRTL,
+        ...newState,
+        canceledLocaleChange: true,
+        previousLocale: null,
+        previousIsRTL: false,
+        locale: state.previousLocale,
+        isRTL: state.previousIsRTL,
+      };
+    }
+    case actions.I18N_SET_CANCEL_FALSE: {
+      return {
+        ...newState,
+        canceledLocaleChange: false,
       };
     }
     default:
-      return state;
+      return newState;
   }
 }
