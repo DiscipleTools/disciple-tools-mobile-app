88391566c89da913ae4aa8cf309579dc56672017
zdmc23 <191707+zdmc23@users.noreply.github.com>
2021-02-08 01:43:43 -0500

    Resolves #474

--- store/sagas/contacts.sagas.js
+++ store/sagas/contacts.sagas.js
@@ -15,8 +15,10 @@ export function* getAll({ domain, token, filter }) {
   yield put({
     type: 'REQUEST',
     payload: {
-      url: `https://${domain}/wp-json/dt-posts/v2/contacts${sharedTools.mapFilterOnQueryParams(
+      url: `https://${domain}/wp-json/dt-posts/v2/contacts${sharedTools.recursivelyMapFilterOnQueryParams(
         newFilter,
+        '',
+        '',
         userData,
       )}`,
       data: {
