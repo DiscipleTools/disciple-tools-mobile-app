372565c55b145d4bb758f532b417927793a45f71
zdmc23 <191707+zdmc23@users.noreply.github.com>
2022-03-05 19:20:37 -0500

    see c748731647968692eb3e0026106d75d5ad2a0e1f

--- screens/Posts/ListScreen.js
+++ screens/Posts/ListScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useReducer, useState } from "react";
+import React from "react";
 
 import OfflineBar from "components/OfflineBar";
 import FilterList from "components/FilterList";
@@ -7,36 +7,13 @@ import { PostItem, PostItemSkeleton, PostItemHidden } from "components/Post/Post
 
 import useFilter from "hooks/useFilter";
 import useList from "hooks/useList";
-import useI18N from "hooks/useI18N";
-import useType from "hooks/useType";
 
 const ListScreen = () => {
 
-  const { i18n } = useI18N();
-  const { isContact, isGroup } = useType();
+  const { defaultFilter, filter, onFilter, search, onSearch } = useFilter();
 
-  const { filter, onFilter } = useFilter();
-
-  // last_modified
-  const defaultFilter = isContact ? {
-    ID: "recent",
-    // TODO: translate?
-    name: "My Recently Viewed",
-    query: { "sort": "-post_date" }
-  } : {
-    ID: "recent",
-    // TODO: translate?
-    name: "My Recently Viewed",
-    query: { "dt_recent": "true" }
-  };
-  // {"ID":"recent","name":"My Recently Viewed","count":0,"subfilter":false,"query":{"dt_recent":true}}
-
-  // initialize default filter
-  useEffect(() => {
-    if (!filter) onFilter(defaultFilter);
-  }, [filter]);
-
-  const { data: items, error, isLoading, isValidating, mutate } = useList();
+  const { data: items, error, isLoading, isValidating, mutate } = useList({ search, filter });
+  // TODO: handler error case
 
   const renderItem = ({ item }) => <PostItem item={item} loading={isLoading||isValidating} mutate={mutate} />;
   //const renderHiddenItem = ({ item }) => <PostItemHidden item={item} loading={isLoading||isValidating} />;
@@ -44,8 +21,6 @@ const ListScreen = () => {
   // TODO: mock search bar, filter tags, FAB, etc..
   const ListSkeleton = () => Array(10).fill(null).map((_, ii) => <PostItemSkeleton key={ii} />);
 
-  // TODO: generic post placeholder with string template for postType
-  const placeholder = isContact ? i18n.t("contactsScreen.noContactPlacheHolder") : i18n.t("groupsScreen.noGroupPlacheHolder");
   return (
     <>
       <OfflineBar />
@@ -54,11 +29,16 @@ const ListScreen = () => {
       ) : (
         <>
           <FilterList
+            sortable
             items={items}
             renderItem={renderItem}
             //renderHiddenItem={renderHiddenItem}
+            search={search}
+            onSearch={onSearch}
+            defaultFilter={defaultFilter}
+            filter={filter}
+            onFilter={onFilter}
             onRefresh={mutate}
-            placeholder={placeholder}
             //leftOpenValue={Constants.SWIPE_BTN_WIDTH * Constants.NUM_SWIPE_BUTTONS_LEFT}
             //rightOpenValue={Constants.SWIPE_BTN_WIDTH * Constants.NUM_SWIPE_BUTTONS_RIGHT}
           />
