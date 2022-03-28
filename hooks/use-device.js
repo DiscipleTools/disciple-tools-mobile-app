56b57b59912eea4479ae64b755b74f1e519e2f69
zdmc23 <191707+zdmc23@users.noreply.github.com>
2022-03-28 04:30:58 -0400

    named Android detection

--- hooks/use-device.js
+++ hooks/use-device.js
@@ -3,7 +3,9 @@ import { Platform } from "react-native";
 // TODO: also use this for Expo Push Notification device info
 const useDevice = () => {
   const isIOS = Platform.OS === "ios";
+  const isAndroid = Platform.OS === "android";
   return {
+    isAndroid,
     isIOS,
   };
 };
