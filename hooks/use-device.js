ae4f6f71752ac80556e1cbaf4eb89911ea0a1269
zdmc23 <191707+zdmc23@users.noreply.github.com>
2022-04-03 22:53:36 -0400

    support determining whether is device or simulator

--- hooks/use-device.js
+++ hooks/use-device.js
@@ -1,10 +1,13 @@
 import { Platform } from "react-native";
 
+import * as Device from 'expo-device';
+
 // TODO: also use this for Expo Push Notification device info
 const useDevice = () => {
   const isIOS = Platform.OS === "ios";
   const isAndroid = Platform.OS === "android";
   return {
+    isDevice: Device.isDevice,
     isAndroid,
     isIOS,
   };
