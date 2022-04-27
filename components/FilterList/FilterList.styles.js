612df6ca37178995cdffe3e95224d0be710494a0
zdmc23 <191707+zdmc23@users.noreply.github.com>
2022-04-26 21:31:57 -0400

    fix broken filter switch, improve filter default selection, styles, check string representation in useEffect dep array

--- components/FilterList/FilterList.styles.js
+++ components/FilterList/FilterList.styles.js
@@ -3,9 +3,13 @@ import Constants from "constants";
 export const localStyles = ({ theme, isRTL, isIOS }) => ({
   container: {
     backgroundColor: theme.surface.primary,
-    height: "100%",
+    height: "110%",
   },
   gutter: {
     marginBottom: Constants.LIST_ITEM_HEIGHT,
-  }
+  },
+  skelton: {
+    backgroundColor: theme.background.primary,
+    height: "100%",
+  },
 });
