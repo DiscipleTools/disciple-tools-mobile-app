a51dbd626ac6a13f37751f345e9ced87aa3915c6
Mike Allbutt <yo@ur.id.au>
2022-04-27 22:40:27 -0500

    added More screen kebab menu

--- screens/MoreScreen.js
+++ screens/MoreScreen.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useLayoutEffect } from "react";
 import { SafeAreaView } from "react-native";
 
 import { ChevronIcon, PostIcon } from "components/Icon";
+import { HeaderRight } from "components/Header/Header";
 import OfflineBar from "components/OfflineBar";
 import ListItem from "components/ListItem";
 
 import useCustomPostTypes from "hooks/use-custom-post-types";
 import useSettings from "hooks/use-settings";
+import useI18N from "hooks/use-i18n";
 import useStyles from "hooks/use-styles";
 
 import { ScreenConstants } from "constants";
@@ -17,6 +19,29 @@ const MoreScreen = ({ navigation }) => {
 
   const { customPostTypes } = useCustomPostTypes() || {};
   const { styles, globalStyles } = useStyles(localStyles);
+  const { i18n, isRTL } = useI18N();
+
+  useLayoutEffect(() => {
+    const kebabItems = [
+      {
+        label: i18n.t("global.viewOnWeb"),
+        urlPath: "#",
+      },
+      {
+        label: i18n.t("global.documentation"),
+        url: `https://disciple.tools/user-docs/disciple-tools-mobile-app/how-to-use/more-screen/`,
+      },
+    ];
+    navigation.setOptions({
+      title: i18n.t("global.more"),
+      headerRight: (props) => (
+        <HeaderRight
+          kebabItems={kebabItems}
+          props
+        />
+      ),
+    });
+  }, []);
 
   const PostButton = ({ type }) => {
     const { settings } = useSettings({ type });
