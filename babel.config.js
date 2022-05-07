d43b0dd713f9bb6226ba771dc1ba6f28dad170c2
zdmc23 <191707+zdmc23@users.noreply.github.com>
2022-05-07 14:07:33 -0400

    see: 782ab87820075837182f70721083115d0d74dbc2

--- babel.config.js
+++ babel.config.js
@@ -7,34 +7,14 @@ module.exports = function(api) {
       [
         'module-resolver',
         {
+          root: ['./'],
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
           alias: {
-            assets: './assets',
-            components: './components',
-            constants: './constants',
-            helpers: './helpers',
-            hooks: './hooks',
-            languages: './languages',
-            navigation: './navigation',
-            screens: './screens',
-            services: './services',
-            shared: './shared',
-            store: './store',
-            storybook: './storybook',
-            utils: './utils',
+            tests: ['./tests/'],
+            "@components": "./components",
           },
-          extensions: [
-            '.js',
-            '.jsx',
-            '.ts',
-            '.tsx',
-            '.android.js',
-            '.android.tsx',
-            '.ios.js',
-            '.ios.tsx',
-          ],
-          //root: ['./']
-        },
-      ],
-    ],
+        }
+      ]
+    ]
   };
 };
